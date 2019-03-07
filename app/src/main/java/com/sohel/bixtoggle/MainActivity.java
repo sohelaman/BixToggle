2bcc3bf51147509f637cacf8f6ce10642bf64c6d
Sohel Aman <sohelamankhan@gmail.com>
2019-03-07 10:19:52 +0600

    Minor updates

--- app/src/main/java/com/sohel/bixtoggle/MainActivity.java
+++ app/src/main/java/com/sohel/bixtoggle/MainActivity.java
@@ -1,13 +1,10 @@
 package com.sohel.bixtoggle;
 
-import android.Manifest;
 import android.content.SharedPreferences;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
